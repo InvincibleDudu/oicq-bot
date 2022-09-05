c43a6ef5b7e653549eb25e921c620f4c17e83a93
InvincibleDudu <invdu@qq.com>
2022-09-05 14:40:16 +0800

    feat: do stuff based on command received from qq

--- src/oicq.ts
+++ src/oicq.ts
@@ -1,7 +1,7 @@
 /**
  * Created by InvincibleDudu on 6/25/2022 at 15:41
  */
-import { AtElem, GroupMessage, ImageElem, MessageElem, PrivateMessage, Sendable } from 'oicq'
+import { AtElem, Group, GroupMessage, ImageElem, MessageElem, PrivateMessage, Sendable, User } from 'oicq'
 import { bugCat, images } from './resource'
 import { countDays, getReadableTime, wait } from './util'
 import { RecurrenceRule } from 'node-schedule'
@@ -73,7 +73,10 @@ let lastMessage: MessageElem[] = []
 
 client.on('message', (e: GroupMessage | PrivateMessage) => {
    console.log(e)
-   if (e instanceof PrivateMessage) return
+   if (e instanceof PrivateMessage) {
+      handlePrivateMessage(e)
+      return
+   }
    duckSent = false
    const msg = e.raw_message
    const atQQList: number[] = e.message.filter((item: any) => item.type === 'at').map((item: any) => item.qq)
@@ -183,4 +186,29 @@ function hasMsgOtherThanAt(msg: MessageElem[]) {
 function compareImage(img1: MessageElem, img2: MessageElem) {
    if (img1.type !== 'image' || img2.type !== 'image') return false
    return img1.file === img2.file
+}
+
+function handlePrivateMessage(msg: PrivateMessage) {
+   if (msg.sender.user_id !== 409174690 && !msg.raw_message.includes('@bot -')) return
+   // @bot -g 748520034 -m foo bar
+   let msgToSend = ''
+   let target: Group | User | undefined = undefined
+   const commands = msg.raw_message.replace('@bot -', '').split('-')
+   for (const command of commands) {
+      const content = command.slice(2)
+      switch (command[0]) {
+         case 'm':
+            msgToSend = content
+            break
+         case 'g':
+            target = client.pickGroup(Number(content))
+            break
+         case 'p':
+            target = client.pickUser(Number(content))
+      }
+   }
+   client.pickUser(409174690).sendMsg('' + msgToSend + ' ' + JSON.stringify(target))
+   if (target && msgToSend) {
+      target.sendMsg(msgToSend)
+   }
 }
